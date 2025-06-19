621ffc1a9a628b9e263521bbaf6cc457321ee3e1
Hassel Alcalá <hassel_aurora_13@hotmail.com>
2025-06-19 17:56:03 -0600

    WIP invalid input

--- py_clob_client/order_builder/builder.py
+++ py_clob_client/order_builder/builder.py
@@ -300,13 +300,14 @@ class OrderBuilder:
         """
         Creates order data with a custom signer (for NEAR Chain Signatures integration)
         """
-        _, maker_amount, taker_amount = self.get_market_order_amounts(
+        enum_side, maker_amount, taker_amount = self.get_market_order_amounts(
             order_args.side,
             order_args.amount,
             order_args.price,
             ROUNDING_CONFIG[options.tick_size],
         )
-        side = order_args.side
+        side = str(enum_side)
+
 
         print(f"\n[DEBUG] Valores antes de crear OrderData:")
         print(f"order_args.token_id: {order_args.token_id} (type: {type(order_args.token_id)})")
